1b95c592d5f400dd1da102298668e59b768b4a81
Patryk <patryk.marcisz@gmail.com>
2020-08-29 09:39:13 +0200

    add security to project

---
+++ src/main/java/com/sda/javakrk24/library/api/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.sda.javakrk24.library.api.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+@Slf4j
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        UserDetails user = User.builder()
+                .username("user@user.pl")
+                .password(passwordEncoder().encode("123"))
+                .build();
+
+        log.info(passwordEncoder().encode("123"));
+
+        auth.inMemoryAuthentication().withUser(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/actuator", "/console*/**").permitAll()
+                .anyRequest().authenticated();
+    }
+}
